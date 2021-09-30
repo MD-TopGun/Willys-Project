96979c81e597dfa22d5a1cd8e5e7948700c7174e
MD-TopGun <81558480+MD-TopGun@users.noreply.github.com>
2021-09-30 11:17:50 +0200

    done

---
+++ README.md
@@ -0,0 +1,20 @@
+npm install### Step 1 - Run npm install
+
+### Step 2 - Replace the old Google Chrome webdriver with a new one
+a) Download the correct Google webdriver (version + operating system) from here:
+  https://sites.google.com/chromium.org/driver/
+
+b) Open the node_modules/chromedriver/libs/chromedriver folder
+   and replace the chromedriver file with the one you just downloaded
+
+### Step 3
+Run your tests by typing:
+* npm run test
+
+### Step 4 - if a graphic report do not open up automatically
+* Goto the reports folder and open the cucumber-report.html file with live server.
+
+### Tips and tricks
+* Install the VSC extension Cucumber (Gherkin) Full support
+  to get syntax highlighting of your .feature files
+
---
+++ step-definitions/Product Category.js
@@ -0,0 +1,23 @@
+// Encapsulate all your step definition in a module exports like below
+module.exports = function () {
+
+    this.When(/^I select "([^"]*)"$/, async function (category) {
+        // Write code here that turns the phrase above into concrete actions
+        await helpers.loadPage('http://www.willys.se');
+        let changeAndConfirmButtons = await driver.findElements(by.css('.gszgQM'));
+        let confirmButton = changeAndConfirmButtons[1];
+        // Click the Accept button
+        await confirmButton.click();
+
+
+
+    });
+
+    this.Then(/^I should see some results$/, async function () {
+        await driver.wait(until.elementsLocated(by.css('div.g')), 10000);
+        let elements = await driver.findElements(by.css('div.g'));
+        // Note that we use expect syntax from Chai.js
+        // https://www.chaijs.com/
+        expect(elements.length).to.not.equal(0);
+    });
+};
