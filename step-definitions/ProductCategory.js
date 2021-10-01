444b62cb1041d0eaff2804d45c3f76e313b3f31b
MD-TopGun <81558480+MD-TopGun@users.noreply.github.com>
2021-10-01 16:23:58 +0200

    commit

--- step-definitions/ProductCategory.js
+++ step-definitions/ProductCategory.js
@@ -1,18 +1,31 @@
 // Encapsulate all your step definition in a module exports like below
 module.exports = function () {
 
-    this.When(/^I select "([^"]*)"$/, async function (category) {
-        // Write code here that turns the phrase above into concrete actions
+    // Match a step definition against a line in Gherkin/the feature file
+    // this.Given or this.When or this.Then + a regular expression
+    // /^ = Start of regular expression
+    // $/ = End of regular expression
+    // "([^"]*)" = match anything inside quotation marks and also
+    // pick it up the text inside the quotation as a parameter for test function
+    this.When(/^I select a category "([^"]*)"$/, async function (searchQuery) {
+        // Navigate to a URL (http://www.willys.se)
         await helpers.loadPage('http://www.willys.se');
-        let changeAndConfirmButtons = await driver.findElements(by.css('.gszgQM'));
+
+        // Google asks you to cahnge or confirm search cookie settings
+        // They do this with buttons with css class .jyfHyd
+        // so grab these two elements, the second one is the Accept button
+        let changeAndConfirmButtons = await driver.findElements(by.css('.onetrust-accept-btn-handler'));
         let confirmButton = changeAndConfirmButtons[1];
         // Click the Accept button
         await confirmButton.click();
 
-
-
+        // Get the search input field
+        let searchInput = await driver.findElement(by.css('input[name="q"]'));
+        // Send our search query as key strokes to it and then press enter
+        await searchInput.sendKeys(searchQuery, selenium.Key.ENTER);
     });
 
+    // See previous description of how to write a step definition function
     this.Then(/^I should see some results$/, async function () {
         await driver.wait(until.elementsLocated(by.css('div.g')), 10000);
         let elements = await driver.findElements(by.css('div.g'));
@@ -20,4 +33,5 @@ module.exports = function () {
         // https://www.chaijs.com/
         expect(elements.length).to.not.equal(0);
     });
+
 };
