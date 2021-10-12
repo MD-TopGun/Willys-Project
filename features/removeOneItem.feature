41af55f15dd49100ae138b106f8494345509a322
Maziar <maziarpak@gmail.com>
2021-10-13 00:01:57 +0200

    User story nr : 8

--- features/removeOneItem.feature
+++ features/removeOneItem.feature
@@ -7,10 +7,9 @@ Feature: Remove an item in the shopping cart
     Given that we are on Willy's website
     And that we accepted the standard cookie policy
     And that we have been through the initial where to deliver popup
-
+    And I have three items in my shopping cart
+    And I write 15 in the input field of the product
   Scenario: Remove an item
-    Given I have three items in my shopping cart
-    And I write 10 in the input field of the product
-    When I regret the first one
-    And I remove the product by cleare in the input field
+    When I regret the first one, I remove the product by cleare in the input field
+    And I write 0 in the input field of the product
     Then the item should not be in the cart any more
---
+++ step-definitions/removeOneItem.js
@@ -0,0 +1,25 @@
+const { $, $$, $$$ } = require('../helpers/element-selection.js');
+let slowDown = true;
+
+async function waitAWhile() {
+  await driver.sleep(slowDown ? 30000 : 1000);
+}
+let productListLength;
+
+module.exports = function () {
+  this.When(/^I regret the first one, I remove the product by cleare in the input field$/, async function () {
+
+    let itemsList = await $$('[class^="ProductListItemstyles__StyledWrapper"]');
+    productListLength = itemsList.length;
+    console.log('\nThe number of different products before deleting:\n', productListLength);
+
+  });
+  this.Then(/^the item should not be in the cart any more$/, async function () {
+    let itemsList = await $$('[class^="ProductListItemstyles__StyledWrapper"]');
+    let itemListLength = itemsList.length;
+    console.log('\nThe number of different products after deleting:\n', itemListLength);
+    expect(productListLength).to.equal(itemListLength + 1);
+
+  });
+
+}
