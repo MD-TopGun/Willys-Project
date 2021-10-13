1a799d0063267b27d68001c3ba6609cc32731b6b
Maziar <maziarpak@gmail.com>
2021-10-13 11:46:20 +0200

    it works properly

--- step-definitions/removeOneItem.js
+++ step-definitions/removeOneItem.js
@@ -2,12 +2,12 @@ const { $, $$, $$$ } = require('../helpers/element-selection.js');
 let slowDown = true;
 
 async function waitAWhile() {
-  await driver.sleep(slowDown ? 30000 : 1000);
+  await driver.sleep(slowDown ? 3000 : 1000);
 }
 let productListLength;
 
 module.exports = function () {
-  this.When(/^I regret the first one, I remove the product by cleare in the input field$/, async function () {
+  this.When(/^I regret the first one, I remove the product by cleare in the input field$ /, async function () {
 
     let itemsList = await $$('[class^="ProductListItemstyles__StyledWrapper"]');
     productListLength = itemsList.length;
