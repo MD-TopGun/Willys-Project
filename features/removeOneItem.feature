e730899b408e0bd15ba08af170cfdd73679861a9
Maziar <maziarpak@gmail.com>
2021-10-14 23:21:20 +0200

    Maziar: Also Remove a product by clicking on minus button tests

--- features/removeOneItem.feature
+++ features/removeOneItem.feature
@@ -8,8 +8,14 @@ Feature: Remove an item in the shopping cart
     And that we accepted the standard cookie policy
     And that we have been through the initial where to deliver popup
     And I have three items in my shopping cart
-    And I write 15 in the input field of the product
-  Scenario: Remove an item
-    When I regret the first one, I remove the product by cleare in the input field
+    And I write 9 in the input field of the product
+
+  Scenario: Remove an item by enter 0 and press return
+    When I regret the first product on the shopping cart
     And I write 0 in the input field of the product
+    Then the item should not be in the cart any more
+
+  Scenario: Remove an item by clicking minus button
+    When I regret the first product on the shopping cart
+    And I decrease the number of same product until zero by clicking on minus button
     Then the item should not be in the cart any more
--- step-definitions/removeOneItem.js
+++ step-definitions/removeOneItem.js
@@ -1,41 +1,36 @@
-const { $, $$, $$$ } = require('../helpers/element-selection.js');
+const { $, $$ } = require('../helpers/element-selection.js');
 let slowDown = true;
 
 async function waitAWhile() {
   await driver.sleep(slowDown ? 5000 : 0);
 }
 let productListLength;
+let productText;
 
 module.exports = function () {
 
   this.When(/^I regret the first product on the shopping cart$/, async function () {
-
     let itemsList = await $$('[class^="ProductListItemstyles__StyledWrapper"]');
     productListLength = itemsList.length;
     console.log('\nThe number of different products before deleting:\n', productListLength);
-
   });
+
   this.When(/^I decrease the number of same product until zero by clicking on minus button$/, async function () {
-    await driver.wait(until.elementsLocated(by.css('button[title="Minska antal"]')), 10000);
     let removeItem = await $('button[title="Minska antal"]');
     let valueAttr = await $('input[aria-label="Ändra produktantal"]').getAttribute('value');
-    //console.log(valueAttr.split(' ')[0]);
-    //let valueAttr = $('.md-3-line input[name="quantity').getAttribute('value');
     let numberOfSameProduct = +valueAttr.split(' ')[0];
-
-    for (let i = 0; i < numberOfSameProduct; i++) {
+    for (let i = 0; i <= numberOfSameProduct; i++) {
       await removeItem.click();
-      driver.sleep(700);
+      driver.sleep(1000);
     }
-    driver.sleep(30000);
     waitAWhile();
   });
   this.Then(/^the item should not be in the cart any more$/, async function () {
-    // await driver.wait(until.elementsLocated(by.css('[class^="ProductListItemstyles__StyledWrapper"]')), 10000);
+    await driver.wait(until.elementsLocated(by.css('[class^="ProductListItemstyles__StyledWrapper"]')), 10000);
     let itemsList = await $$('[class^="ProductListItemstyles__StyledWrapper"]');
     let itemListLength = itemsList.length;
     console.log('\nThe number of different products after deleting:\n', itemListLength);
-    //expect(productListLength).to.equal(itemListLength + 1);
+    expect(productListLength).to.equal(itemListLength + 1);
     waitAWhile();
   });
 
